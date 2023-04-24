b5ed0b93bc2261f444d6f47e4f39eb5b42f0357f
OleksandrGnatiuk <oleksandr.gnatiuk@gmail.com>
2023-04-25 02:44:59 +0300

    first step

---
+++ hw_quotes/quotes/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.core.paginator import Paginator
+
+from .utils import get_mongodb
+
+
+def main(request):
+    db = get_mongodb()
+    quotes = db.quotes.find()
+    return render(request, 'quotes/index.html', context={'quotes': quotes})
